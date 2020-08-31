a350f8bcf5bb3285f48290cfc71c0e8a50fade5f
vanhnguyen1006 <0ozstarzo0@gmail.com>
2020-08-31 13:52:53 +0700

    update js show pass

--- js/login.js
+++ js/login.js
@@ -13,4 +13,18 @@
         showBtn.style.color = "#222";
       }
     });
-   					
+           
+    const pass_field2 = document.querySelector('.pass-key-2');
+    const showBtn2 = document.querySelector('.show-2');
+    showBtn2.addEventListener('click', function(){
+      if(pass_field2.type == "password"){
+        pass_field2.type = "text";
+        showBtn2.textContent = "HIDE";
+        showBtn2.style.color = "#3498db";
+      }
+      else{
+        pass_field2.type = "password";
+        showBtn2.textContent = "SHOW";
+        showBtn2.style.color = "#222";
+      }
+    });
